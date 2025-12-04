0ccd04046a11f683a350effd64cf53837b37e518
nicunursekatie <katielong2316@gmail.com>
2025-12-03 21:28:55 -0500

    set up github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tsp-host-onboarding/', // Update this to match your GitHub repo name
+  base: '/tsphosttoolkit/', // GitHub Pages base path
 })
